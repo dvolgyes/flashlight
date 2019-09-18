519ae0ca8f4d64255d0282b6d1cbb80e22b1d6aa
David Völgyes <david.volgyes@ieee.org>
2019-09-18 15:30:19 +0200

    fixes

--- flashlight/util/__init__.py
+++ flashlight/util/__init__.py
@@ -5,6 +5,7 @@
 from .boxes import leaf_values, resolve_environment, resolve_templates
 from .reproducibility import ensure_reproducible_results
 from .gittools import git_untracked, git_changes, git_summary, git_synchronize
-
+from .utils import dcn, dcnm
 __all__ = ['leaf_values', 'resolve_environment', 'resolve_templates', 'ensure_reproducible_results',
-           'git_untracked', 'git_changes', 'git_summary', 'git_synchronize']
+           'git_untracked', 'git_changes', 'git_summary', 'git_synchronize',
+           'dcn', 'dcnm']
